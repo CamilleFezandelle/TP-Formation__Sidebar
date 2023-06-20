8179fb3c8e7b02362cd2bd94c2e613456e05a77f
Camille Dev <fezandellecamille@gmail.com>
2023-06-20 17:27:02 +0200

    realization and finalization of project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+const sideBar = document.getElementById('side-bar');
+const sideBtn = document.getElementById('btn');
+const content = document.querySelector('.content');
 
-// Ranger la sidebar si on click sur le contenu principal
+sideBtn.addEventListener('click', () => {
+	sideBar.classList.toggle('active');
+});
 
-// BONUS
-// Créer en css l'hamburger menu qui se transforme en croix
-
-// Aide
-// Priorités en CSS : id > classe > baliseHtml
+content.addEventListener('click', () => {
+	sideBar.classList.remove('active');
+});
